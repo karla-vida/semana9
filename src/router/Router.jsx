2f895fb9f99aaff00dc69cf9dba0b440bfe394ce
karlacvida <karlacvida@hotmail.com>
2022-08-30 00:28:42 -0300

    exercicio7

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { Produtos } from '@pages';
+import { Produtos, ProdutosSelecionados } from '@pages';
 
 export const Router = () => {
   return (
     <Routes>
       <Route path='/produtos' element={<Produtos />} />
+      <Route path='/produtos-selecionados' element={<ProdutosSelecionados />} />
       <Route path='*' element={<Navigate to={'/produtos'} replace={true} />} />
     </Routes>
   );
