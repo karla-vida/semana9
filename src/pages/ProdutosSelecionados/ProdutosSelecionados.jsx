4f72a1cbb09f000127bbfdd8f7aa51331b86bf02
karlacvida <karlacvida@hotmail.com>
2022-09-06 20:27:38 -0300

    exercicio9

--- src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
+++ src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
@@ -2,13 +2,17 @@ import { ListaCards } from '@components';
 import { useProdutosSelecionados } from '@contexts';
 
 export const ProdutosSelecionados = () => {
-  const { produtosSelecionados } = useProdutosSelecionados();
+  const { produtosSelecionados, limparProdutos } = useProdutosSelecionados();
 
+  const total = produtosSelecionados.reduce((soma, item) => {
+    return soma + item.valor;
+  }, 0);
 
   return (
     <>
         <h1>Produtos Selecionados</h1>
-      <ListaCards produtos={produtosSelecionados} />
+        <h2> {`R$ ${total.toFixed(2).replace('.', `,`)}`}</h2>
+        <ListaCards produtos={produtosSelecionados} />
     </>
   );
 };
